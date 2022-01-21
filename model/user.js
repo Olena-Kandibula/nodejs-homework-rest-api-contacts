52d7948367bdbd9edc49826ab8870351165d23eb
Olena-Kandibula <bakkara@ukr.net>
2022-01-21 16:43:39 +0200

    hw 05 - avatars

--- model/user.js
+++ model/user.js
@@ -27,6 +27,10 @@ const userSchema = Schema({
   token: {
     type: String,
     default: null,
+    },
+   avatarURL: {
+    type: String,
+    default: ''
   },
 },
     { versionKey: false, timestamps: true }
